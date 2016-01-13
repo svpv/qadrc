193b4908c0f9014ca613e9fd609613d3c2bbe910
Alexey Tourbin <alexey.tourbin@gmail.com>
2016-01-13 09:58:12 +0300

    af_qalimiter.c: rewrite to work on frame boundary

--- af_qalimiter.c
+++ af_qalimiter.c
@@ -1,111 +1,337 @@
 #include <stddef.h>
 #include <math.h>
 #include "libavutil/channel_layout.h"
+#include "libavutil/avassert.h"
 #include "avfilter.h"
 #include "audio.h"
 #include "internal.h"
 
-typedef struct QALimiterContext {
-    float *buffer[8];
-    size_t bufsiz[8];
-    size_t processed[8];
-} QALimiterContext;
-
 #define m_thresh 0.9921875f
 
-#define m_buffer s->buffer
-#define m_bufsiz s->bufsiz
-#define m_processed s->processed
+/* fix a single spike between frames[fi1][f1_pos] and frames[fi2][f2_end] */
+static void fix_spike1(AVFilterLink *inlink, AVFrame **frames, int ch,
+	int fi1, size_t f1_pos, int fi2, size_t f2_end, float peak)
+{
+    float xpeak = peak;
+    peak = fabsf(peak);
+
+    for (int fi = fi1; fi <= fi2; fi++) {
+	AVFrame *frame = frames[fi], *out_frame;
+	if (av_frame_is_writable(frame))
+	    out_frame = frame;
+	else {
+	    out_frame = ff_get_audio_buffer(inlink, frame->nb_samples);
+	    av_frame_copy_props(out_frame, frame);
+	    av_frame_copy(out_frame, frame);
+        }
+
+	size_t begin = (fi == fi1) ? f1_pos : 0;
+	size_t end = (fi == fi2) ? f2_end : frame->nb_samples;
+	float *x = (float *) out_frame->extended_data[ch];
+
+	if (peak < m_thresh * 2.0) {
+	    float a = (peak - m_thresh) / (peak * peak);
+	    if (xpeak > 0) a = - a;
+	    for (size_t i = begin; i < end; ++i)
+		x[i] = x[i] + a * x[i] * x[i];
+	}
+	else {
+	    float u = peak, v = m_thresh;
+	    float a = (u - 2 * v) / (u * u * u);
+	    float b = (3 * v - 2 * u) / (u * u);
+	    if (xpeak < 0)
+		b *= -1.0;
+	    for (size_t i = begin; i < end; ++i)
+		x[i] = x[i] + b * x[i] * x[i] + a * x[i] * x[i] * x[i];
+	}
+
+	if (out_frame != frame) {
+	    av_frame_free(&frame);
+	    frames[fi] = out_frame;
+	}
+    }
+}
+
+/* search for a peak (above threshold) */
+static void find_peak(AVFrame **frames, int ch,
+	int fi1, size_t f1_pos, int fi2, size_t f2_end,
+	int *peak_fi, size_t *peak_pos, float *peak_val)
+{
+    for (int fi = fi1; fi <= fi2; fi++) {
+	AVFrame *frame = frames[fi];
+
+	size_t begin = (fi == fi1) ? f1_pos : 0;
+	size_t end = (fi == fi2) ? f2_end : frame->nb_samples;
+	float *x = (float *) frame->extended_data[ch];
+
+	size_t i;
+	for (i = begin; i < end; i++)
+	    if (x[i] > m_thresh || x[i] < -m_thresh)
+		break;
+	if (i == end)
+		continue;
+
+	/* found peak */
+	*peak_fi = fi;
+	*peak_pos = i;
+	*peak_val = x[i];
+	return;
+    }
+}
+
+/* when a peak is found, search backwards for spike start */
+static void find_spike_start(AVFrame **frames, int ch,
+	int peak_fi, size_t peak_pos,
+	int *start_fi, size_t *start_pos,
+	float peak_val)
+{
+    for (int fi = peak_fi; fi >= 0; fi--) {
+	AVFrame *frame = frames[fi];
+
+	ssize_t pos = (fi == peak_fi) ? peak_pos : frame->nb_samples;
+	float *x = (float *) frame->extended_data[ch];
+
+	if (peak_val < 0) {
+	    if (x[0] < 0)
+		while (pos >= 0 && x[pos] < 0)
+		    pos--;
+	    else
+		while (x[pos] < 0)
+		    pos--;
+	}
+	else {
+	    if (x[0] > 0)
+		while (pos >= 0 && x[pos] > 0)
+		    pos--;
+	    else
+		while (x[pos] > 0)
+		    pos--;
+	}
+	if (pos < 0)
+	    continue;
+	/* found intersection */
+	*start_fi = fi;
+	*start_pos = pos + 1;
+	return;
+    }
+    /* assume leftmost */
+    *start_fi = 0;
+    *start_pos = 0;
+}
+
+/* search to the end of the spike and update peak value */
+static void find_spike_end(AVFrame **frames, int ch,
+	int peak_fi, size_t peak_pos, int fi2,
+	int *end_fi, size_t *end_end,
+	float *peak_val)
+{
+    for (int fi = peak_fi; fi <= fi2; fi++) {
+	AVFrame *frame = frames[fi];
+	size_t begin = (fi == peak_fi) ? peak_pos : 0;
+	size_t end = frame->nb_samples;
+	float *x = (float *) frame->extended_data[ch];
+
+	size_t i = begin;
+	float p = *peak_val;
+
+	if (p < 0) {
+	    /* the spike is below x-axis */
+	    if (x[end-1] < 0) {
+		for (; i < end && x[i] < 0; i++)
+		    if (x[i] < p)
+			p = x[i];
+	    }
+	    else {
+		for (; x[i] < 0; i++)
+		    if (x[i] < p)
+			p = x[i];
+	    }
+	}
+	else {
+	    /* the spike is above x-axis */
+	    if (x[end-1] > 0) {
+		for (; i < end && x[i] > 0; i++)
+		    if (x[i] > p)
+			p = x[i];
+	    }
+	    else {
+		for (; x[i] > 0; i++)
+		    if (x[i] > p)
+			p = x[i];
+	    }
+	}
+
+	*peak_val = p;
+
+	if (i == end)
+	    continue;
+	/* found intersection */
+	*end_fi = fi;
+	*end_end = i;
+	return;
+    }
+    /* assume rightmost */
+    *end_fi = fi2;
+    *end_end = frames[fi2]->nb_samples;
+}
+
+static void fix_spikes(AVFilterLink *inlink, AVFrame **frames, int ch,
+	int fi1, size_t f1_pos, int fi2, size_t f2_end)
+{
+    while (1) {
+	/* find peak */
+	int peak_fi;
+	size_t peak_pos;
+	float peak_val = 0;
+	find_peak(frames, ch, fi1, f1_pos, fi2, f2_end,
+		&peak_fi, &peak_pos, &peak_val);
+	if (peak_val == 0)
+	    return;
+
+	/* find spike start */
+	int start_fi;
+	size_t start_pos;
+	find_spike_start(frames, ch, peak_fi, peak_pos,
+		&start_fi, &start_pos, peak_val);
+
+	/* find spike end */
+	int end_fi;
+	size_t end_end;
+	find_spike_end(frames, ch, peak_fi, peak_pos, fi2,
+		&end_fi, &end_end, &peak_val);
+
+	/* fix the spike */
+	fix_spike1(inlink, frames, ch,
+		start_fi, start_pos, end_fi, end_end, peak_val);
+
+	if (end_fi == fi2 && end_end == f2_end)
+	    break;
+	fi1 = end_fi;
+	f1_pos = end_end;
+    }
+}
+
+/* find the end limit up to which the buffer can be processed;
+ * that is, up to the last intersection with x-axis */
+static size_t find_channel_end(AVFrame *frame, int ch)
+{
+    float *x = (float *) frame->extended_data[ch];
+    ssize_t pos = frame->nb_samples - 1;
+    if (x[pos] < 0) {
+	if (x[0] < 0)
+	    do
+		pos--;
+	    while (pos >= 0 && x[pos] < 0);
+	else
+	    do
+		pos--;
+	    while (x[pos] < 0);
+    }
+    else if (x[pos] > 0) {
+	if (x[0] > 0)
+	    do
+		pos--;
+	    while (pos >= 0 && x[pos] > 0);
+	else
+	    do
+		pos--;
+	    while (x[pos] > 0);
+    }
+    return pos + 1;
+}
+
+typedef struct QALimiterContext {
+    AVFrame **frames;
+    size_t nframes;
+    int fi[8]; /* frame index, per channel */
+    size_t fpos[8]; /* position in the frame up to which the input has been processed */
+} QALimiterContext;
+
+static int flush_frames(AVFilterContext *ctx, QALimiterContext *s, int nch)
+{
+    int fiend = s->fi[0];
+    for (int ch = 1; ch < nch; ch++)
+	if (s->fi[ch] < fiend)
+	    fiend = s->fi[ch];
+
+    if (fiend == 0)
+	return 0;
+
+    for (int fi = 0; fi < fiend; fi++)
+	ff_filter_frame(ctx->outputs[0], s->frames[fi]);
+
+    s->nframes -= fiend;
+    memmove(s->frames, s->frames + fiend, s->nframes * sizeof(AVFrame *));
+
+    for (int ch = 0; ch < nch; ch++)
+	s->fi[ch] -= fiend;
+
+    return 0;
+}
 
 static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 {
     AVFilterContext *ctx = inlink->dst;
     QALimiterContext *s = ctx->priv;
 
-    const int nin = frame->nb_samples;
-    const int m_nchannels = inlink->channels;
-    for (int n = 0; n < m_nchannels; ++n) {
-	// fill the buffer
-	float *x = m_buffer[n] = av_realloc_f(m_buffer[n], m_bufsiz[n] + nin, sizeof(float));
-	memcpy(x + m_bufsiz[n], frame->extended_data[n], nin * sizeof(float));
-	m_bufsiz[n] += nin;
-	// find the end limit up to which the buffer can be processed;
-	// that is, up to the last intersection with x-axis
-	ssize_t limit = m_bufsiz[n];
-	if (limit > 0 && nin > 0) {
-	    float last = x[limit-1];
-	    for (; limit > 0 && x[limit-1] * last > 0; --limit)
-		;
+    s->frames = av_realloc_f(s->frames, s->nframes + 1, sizeof(AVFrame *));
+    s->frames[s->nframes++] = frame;
+
+    int nch = inlink->channels;
+    for (int ch = 0; ch < nch; ch++) {
+	size_t end = find_channel_end(frame, ch);
+	if (end == 0)
+	    // no intersection with x-axis
+	    continue;
+	int full = (end == frame->nb_samples);
+	fix_spikes(inlink, s->frames, ch,
+		s->fi[ch], s->fpos[ch], s->nframes - 1, end);
+	if (full) {
+	    s->fi[ch] = s->nframes;
+	    s->fpos[ch] = 0;
 	}
-	// recall the last limit, the end at which we stopped last time
-	ssize_t end = m_processed[n];
-	// fix the spikes between the last end and the new end
-	while (end < limit) {
-	    ssize_t peak_pos = end;
-	    for (; peak_pos < limit; ++peak_pos)
-		if (x[peak_pos] > m_thresh || x[peak_pos] < -m_thresh)
-		    break;
-	    if (peak_pos == limit)
-		break;
-	    ssize_t start = peak_pos;
-	    float peak = fabsf(x[peak_pos]);
-
-	    while (start > end && x[peak_pos] * x[start] >= 0)
-		--start;
-	    ++start;
-	    for (end = peak_pos + 1; end < limit; ++end) {
-		if (x[peak_pos] * x[end] < 0)
-		    break;
-		float y = fabsf(x[end]);
-		if (y > peak) {
-		    peak = y;
-		    peak_pos = end;
-		}
-	    }
-	    if (peak < m_thresh * 2.0) {
-		float a = (peak - m_thresh) / (peak * peak);
-		if (x[peak_pos] > 0) a = - a;
-		for (ssize_t i = start; i < end; ++i)
-		    x[i] = x[i] + a * x[i] * x[i];
-	    } else {
-		float u = peak, v = m_thresh;
-		float a = (u - 2 * v) / (u * u * u);
-		float b = (3 * v - 2 * u) / (u * u);
-		if (x[peak_pos] < 0)
-		    b *= -1.0;
-		for (ssize_t i = start; i < end; ++i)
-		    x[i] = x[i] + b * x[i] * x[i] + a * x[i] * x[i] * x[i];
-	    }
-	}
-	m_processed[n] = limit;
-    }
-    size_t prod = 2 * nin;
-    for (int n = 0; n < m_nchannels; ++n)
-	if (m_processed[n] < prod)
-	    prod = m_processed[n];
-
-    if (prod < 1) {
-	av_frame_free(&frame);
-	return 0;
-    }
-
-    AVFrame *out_frame = ff_get_audio_buffer(inlink, prod);
-    av_frame_copy_props(out_frame, frame);
-    av_frame_free(&frame);
-
-    for (int n = 0; n < m_nchannels; ++n)
-	memcpy(out_frame->extended_data[n], s->buffer[n], prod * sizeof(float));
-
-    for (int n = 0; n < m_nchannels; ++n) {
-	float *x = m_buffer[n];
-	if (m_bufsiz[n] && prod) {
-	    memmove(x, x + prod, (m_bufsiz[n] - prod) * sizeof(float));
-	    m_bufsiz[n] -= prod;
-	    m_processed[n] -= prod;
+	else {
+	    s->fi[ch] = s->nframes - 1;
+	    s->fpos[ch] = end;
 	}
     }
 
-    return ff_filter_frame(ctx->outputs[0], out_frame);
+    return flush_frames(ctx, s, nch);
+}
+
+static int final_flush(AVFilterLink *inlink, AVFilterContext *ctx, QALimiterContext *s)
+{
+    int nch = inlink->channels;
+    for (int ch = 0; ch < nch; ch++) {
+	if (s->fi[ch] == s->nframes)
+	    continue;
+	fix_spikes(inlink, s->frames, ch,
+		s->fi[ch], s->fpos[ch], s->nframes - 1,
+		s->frames[s->nframes-1]->nb_samples - 1);
+	s->fi[ch] = s->nframes;
+	s->fpos[ch] = 0;
+    }
+    return flush_frames(ctx, s, nch);
+}
+
+static int request_frame(AVFilterLink *outlink)
+{
+    AVFilterContext *ctx = outlink->src;
+    QALimiterContext *s = ctx->priv;
+
+    int ret = ff_request_frame(ctx->inputs[0]);
+    if (ret == AVERROR_EOF && !ctx->is_disabled && s->nframes)
+	ret = final_flush(outlink, ctx, s);
+
+    return ret;
+}
+
+static av_cold void uninit(AVFilterContext *ctx)
+{
+    QALimiterContext *s = ctx->priv;
+    for (int i = 0; i < s->nframes; i++)
+	av_frame_free(&s->frames[i]);
+    av_freep(&s->frames);
 }
 
 static int query_formats(AVFilterContext *ctx)
@@ -113,7 +339,7 @@ static int query_formats(AVFilterContext *ctx)
     AVFilterFormats *formats = NULL;
     AVFilterChannelLayouts *layouts;
 
-    ff_add_format(&formats, AV_SAMPLE_FMT_FLT);
+    ff_add_format(&formats, AV_SAMPLE_FMT_FLTP);
     ff_set_common_formats(ctx, formats);
 
     layouts = ff_all_channel_layouts();
@@ -125,19 +351,27 @@ static int query_formats(AVFilterContext *ctx)
     return 0;
 }
 
+static int config_output(AVFilterLink *outlink)
+{
+    outlink->flags |= FF_LINK_FLAG_REQUEST_LOOP;
+    return 0;
+}
+
 static const AVFilterPad inputs[] = {
     {
-        .name         = "default",
-        .type         = AVMEDIA_TYPE_AUDIO,
-        .filter_frame = filter_frame,
+        .name          = "default",
+        .type          = AVMEDIA_TYPE_AUDIO,
+        .filter_frame  = filter_frame,
     },
     { NULL }
 };
 
 static const AVFilterPad outputs[] = {
     {
-        .name = "default",
-        .type = AVMEDIA_TYPE_AUDIO,
+        .name          = "default",
+        .type          = AVMEDIA_TYPE_AUDIO,
+        .config_props  = config_output,
+        .request_frame = request_frame,
     },
     { NULL }
 };
@@ -145,6 +379,7 @@ static const AVFilterPad outputs[] = {
 AVFilter ff_af_qalimiter = {
     .name          = "qalimiter",
     .description   = NULL_IF_CONFIG_SMALL("qaac soft limiter"),
+    .uninit        = uninit,
     .query_formats = query_formats,
     .inputs        = inputs,
     .outputs       = outputs,
