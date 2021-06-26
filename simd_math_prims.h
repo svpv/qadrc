27abf39b563af4a07a1419259d6c1c143ec11bed
Alexey Tourbin <alexey.tourbin@gmail.com>
2021-06-26 13:58:10 +0300

    simd_math_prims.h: sync with jhjourdan, removed sin/cos

--- simd_math_prims.h
+++ simd_math_prims.h
@@ -28,58 +28,56 @@ SOFTWARE.
 #define SIMD_MATH_PRIMS_H
 
 #include <math.h>
+#include <stdint.h>
 
 /* Workaround a lack of optimization in gcc */
-static float exp_cst1 = 2139095040.f;
-static float exp_cst2 = 0.f;
+float exp_cst1_f = 2139095040.f;
+float exp_cst2_f = 0.f;
 
 /* Relative error bounded by 1e-5 for normalized outputs
    Returns invalid outputs for nan inputs
    Continuous error */
 static inline float expapprox(float val)
 {
-  union { int i; float f; } xu, xu2;
+  union { int32_t i; float f; } xu, xu2;
   float val2, val3, val4, b;
-  int val4i;
+  int32_t val4i;
   val2 = 12102203.1615614f*val+1065353216.f;
-  val3 = val2 < exp_cst1 ? val2 : exp_cst1;
-  val4 = val3 > exp_cst2 ? val3 : exp_cst2;
-  val4i = (int) val4;
+  val3 = val2 < exp_cst1_f ? val2 : exp_cst1_f;
+  val4 = val3 > exp_cst2_f ? val3 : exp_cst2_f;
+  val4i = (int32_t) val4;
   xu.i = val4i & 0x7F800000;
   xu2.i = (val4i & 0x7FFFFF) | 0x3F800000;
   b = xu2.f;
 
   /* Generated in Sollya with:
      > f=remez(1-x*exp(-(x-1)*log(2)),
-               [|1,(x-1)*(x-2), (x-1)*(x-2)*x, (x-1)*(x-2)*x*x|],
-               [1,2], exp(-(x-1)*log(2)));
+               [|(x-1)*(x-2), (x-1)*(x-2)*x, (x-1)*(x-2)*x*x|],
+               [1.000001,1.999999], exp(-(x-1)*log(2)));
      > plot(exp((x-1)*log(2))/(f+x)-1, [1,2]);
      > f+x;
   */
   return
-    xu.f * (0.510397365625862338668154f + b *
-            (0.310670891004095530771135f + b *
-             (0.168143436463395944830000f + b *
-              (-2.88093587581985443087955e-3f + b *
-               1.3671023382430374383648148e-2f))));
+    xu.f * (0.509871020f + b * (0.312146713f + b * (0.166617139f + b *
+              (-2.190619930e-3f + b * 1.3555747234e-2f))));
 }
 
-/* Absolute error bounded by 1e-6 for normalized inputs
+/* Absolute error bounded by 1e-5 for normalized inputs
    Returns a finite number for +inf input
    Returns -inf for nan and <= 0 inputs.
    Continuous error. */
 static inline float logapprox(float val)
 {
-  union { float f; int i; } valu;
+  union { float f; int32_t i; } valu;
   float exp, addcst, x;
   valu.f = val;
   exp = valu.i >> 23;
-  /* 89.970756366f = 127 * log(2) - constant term of polynomial */
+  /* -89.970756366f = -127 * log(2) + constant term of polynomial bellow. */
   addcst = val > 0 ? -89.970756366f : -(float)INFINITY;
   valu.i = (valu.i & 0x7FFFFF) | 0x3F800000;
   x = valu.f;
 
-  /* Generated in Sollya using :
+  /* Generated in Sollya using:
     > f = remez(log(x)-(x-1)*log(2),
             [|1,(x-1)*(x-2), (x-1)*(x-2)*x, (x-1)*(x-2)*x*x,
               (x-1)*(x-2)*x*x*x|], [1,2], 1, 1e-8);
@@ -87,40 +85,9 @@ static inline float logapprox(float val)
     > f+(x-1)*log(2)
   */
   return
-    x * (3.529304993f + x * (-2.461222105f +
-      x * (1.130626167f + x * (-0.288739945f +
-        x * 3.110401639e-2f))))
-    + (addcst + 0.69314718055995f*exp);
+    x * (3.529304993f + x * (-2.461222105f + x * (1.130626167f +
+      x * (-0.288739945f + x * 3.110401639e-2f))))
+    + (addcst + 0.6931471805f*exp);
 }
-
-#if 0
-/* Correct only in [-pi, pi]
-   Absolute error bounded by 5e-5
-   Continuous error */
-static inline float cosapprox(float val)
-{
-  float val2 = val*val;
-  return
-    0.999959766864776611328125f + val2 *
-    (-0.4997930824756622314453125f + val2 *
-     (4.1496001183986663818359375e-2f + val2 *
-      (-1.33926304988563060760498046875e-3f + val2 *
-       1.8791708498611114919185638427734375e-5f)));
-}
-
-/* Correct only in [-pi, pi]
-   Absolute error bounded by 6e-6
-   Continuous error */
-static inline float sinapprox(float val)
-{
-  float val2 = val*val;
-  return
-    val * (0.99997937679290771484375f + val2 *
-           (-0.166624367237091064453125f + val2 *
-            (8.30897875130176544189453125e-3f + val2 *
-             (-1.92649182281456887722015380859375e-4f + val2 *
-              2.147840177713078446686267852783203125e-6f))));
-}
-#endif
 
 #endif
